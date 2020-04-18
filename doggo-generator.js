c944a6b9823f89c763b26e24f12339ca3d5d79ef
Allison Bates <allisonkbates@gmail.com>
2020-04-17 22:30:22 -0400

    add dogs

--- doggo-generator.js
+++ doggo-generator.js
@@ -18,11 +18,37 @@ var getDoggos = function() {
 	}).then(function(response) {
 		return response.json();
 	}).then(function(data) {
-		console.log('dogs', data);
+		var doggos = data.animals;
+		doggos.forEach(function (doggo){
+			var largePhoto = doggo && doggo['photos'][0] && doggo['photos'][0]['medium'] ? doggo['photos'][0]['medium'] : "cheyenne/cheyenne_hero.jpg";
+			console.log(doggo);
+			function createCard() {
+				var card = document.createElement('div');
+				card.setAttribute('class', 'card');
+				var dogimg = document.createElement('img');
+				dogimg.src=largePhoto;
+				dogimg.setAttribute('class', 'dog-img-card');
+				card.appendChild(dogimg);
+				var badge = document.createElement('h2');
+				card.appendChild(badge);
+				badge.setAttribute('class', 'badge');
+				var dogName = document.createTextNode(doggo.name);
+				badge.appendChild(dogName);
+				var dogInfo = document.createElement('div');
+				dogInfo.setAttribute('class', 'dog-info');
+				//var dogDescription = document.createElement('p')
+				//dogDescription.createTextNode(doggo.description);
+				//doginfo.appendChild(dogDescription);
+				document.getElementById('cards').appendChild(card);
+			};
+			createCard();
+		});
 	}).catch(function(err){
 		console.log('something went wrong...', err);
 	});
 };
+
+
 
 // get oAuth creds
 var getOAuth = function() {
@@ -42,36 +68,7 @@ var getOAuth = function() {
 	}).catch(function(err) {
 		console.log('something went wrong...', err);
 	});
-};
-
-// api call for oAuth 2
-	
-fetch('https://api.petfinder.com/v2/oauth2/token', {
-	method: 'POST',
-	body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret,
-	headers: {
-		'Content-Type': 'application/x-www-form-urlencoded'
-	}
-}).then(function(response) {
-	return response.json();
-}).then(function(data) {
-	console.log('token', data);
-	return fetch('https://api.petfinder.com/v2/animals?size=' + size + '&type=' + type + '&status=' + status, {
-		headers: {
-			'Authorization': data.token_type + ' ' + data.access_token,
-			'Content-Type': 'application/x-www-form-urlencoded'
-		}
-	});
-}).then(function(response) {
-	return response.json();
-}).then(function(data) {
-	var name = data['animals'][0]['name'];
-	document.getElementById('badge').innerHTML=name;
-	console.log(data['animals'][0]['name']);
-
-}).catch(function(err) {
-	console.log('something went wrong... ', err);
-});
+};	
 
 // check token & get pets
 var callDoggos = function() {
@@ -82,5 +79,8 @@ var callDoggos = function() {
 		});
 		return;
 	}
+	console.log('from cache');
 	getDoggos();
 };
+
+callDoggos();
